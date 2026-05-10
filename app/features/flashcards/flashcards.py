85d37b8d6cb19cfff8edf35a7699bb4e9df84806
thiagobgarc <thiagobuenogarcia1@gmail.com>
2026-05-10 03:01:03 -0400

    Added new feature that keeps score and even gives percentage. Also took some code out to write cleaner and more precise code.

--- app/features/flashcards/flashcards.py
+++ app/features/flashcards/flashcards.py
@@ -4,8 +4,8 @@ from app.services.services import load_words
 class Flashcard:
     def __init__(self):
         self.words = load_words()
-        self.score = 0
-        self.total_questions = 0
+        self.correct_answers = 0
+        self.incorrect_answers = 0
 
     
     def get_random_word(self):
@@ -18,14 +18,31 @@ class Flashcard:
         input("\nPress Enter to see answer...")
 
         print(f"English: {card['english']}")
+
+        answer = input("\nDid you get it right? (y/n): ")
+        if answer.lower() == 'y':
+            self.correct_answers += 1
+        else:
+            self.incorrect_answers += 1
+
+    def get_score(self):
+        total = self.correct_answers + self.incorrect_answers
+
+        if total == 0:
+            return 0
+        else:
+            accuracy = (self.correct_answers / total) * 100
+
+        print("]n=== SCORE ===")
+        print(f"Correct: {self.correct_answers}")
+        print(f"Incorrect: {self.incorrect_answers}")
+        print(f"Accuracy: {accuracy:.2f}%")
     
     def start_flashcards(self):
         while True:
             card = self.get_random_word()
             self.display_card(card)
-            self.total_questions += 1
             again = input("\nContinue? (y/n): ")
             if again.lower() != 'y':
                 break
-            else:
-                return self.score, self.total_questions
+        self.get_score()
