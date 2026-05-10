6899e6db1db810c283bf3f9854e72811af6216fa
thiagobgarc <thiagobuenogarcia1@gmail.com>
2026-05-09 20:39:29 -0400

    Created Flashcard class w/ randomization of word, display, and start flash card w/ some functionality

---
+++ app/features/flashcards/flashcards.py
@@ -0,0 +1,31 @@
+import random
+from app.services.services import load_words
+
+class Flashcard:
+    def __init__(self, words, score, total_questions):
+        self.words = words
+        self.score = score
+        self.total_questions = total_questions
+
+    
+    def get_random_word(self):
+        return random.choice(self.words)
+    
+    def display_card(self, card):
+        print("\n=== FLASH CARD ===")
+        print(f"Portuguese: {card['portuguese']}")
+
+        input("\nPress Enter to see answer...")
+
+        print(f"English: {card['English']}")
+    
+    def start_flashcards(self):
+        while True:
+            card = self.get_random_word()
+            self.display_card(card)
+            self.total_questions += 1
+            again = input("\nContinue? (y/n): ")
+            if again.lower() != 'y':
+                break
+            else:
+                return self.score, self.total_questions
