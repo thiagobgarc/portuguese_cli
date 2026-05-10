1de80cb03107f600f44cb1b4ccc31cd91a7de362
thiagobgarc <thiagobuenogarcia1@gmail.com>
2026-05-09 20:09:14 -0400

    Added some mock vocab words insinside data/vocabulary/vocaulary.py as JSON

---
+++ data/vocabulary/vocabulary.py
@@ -0,0 +1,110 @@
+[
+    {
+        "portuguese": "Olá",
+        "english": "Hello"
+    },
+    {
+        "portuguese": "Tchau",
+        "english": "Goodbye"
+    },
+    {
+        "portuguese": "Obrigado",
+        "english": "Thank you"
+    },
+    {
+        "portuguese": "Por favor",
+        "english": "Please"
+    },
+    {
+        "portuguese": "Desculpe",
+        "english": "Sorry"
+    },
+    {
+        "portuguese": "Sim",
+        "english": "Yes"
+    },
+    {
+        "portuguese": "Não",
+        "english": "No"
+    },
+    {
+        "portuguese": "Amigo",
+        "english": "Friend"
+    },
+    {
+        "portuguese": "Família",
+        "english": "Family"
+    },
+    {
+        "portuguese": "Casa",
+        "english": "House"
+    },
+    {
+        "portuguese": "Comida",
+        "english": "Food"
+    },
+    {
+        "portuguese": "Água",
+        "english": "Water"
+    },
+    {
+        "portuguese": "Trabalho",
+        "english": "Work"
+    },
+    {
+        "portuguese": "Escola",
+        "english": "School"
+    },
+    {
+        "portuguese": "Carro",
+        "english": "Car"
+    },
+    {
+        "portuguese": "Cidade",
+        "english": "City"
+    },
+    {
+        "portuguese": "País",
+        "english": "Country"
+    },
+    {
+        "portuguese": "Mundo",
+        "english": "World"
+    },
+    {
+        "portuguese": "Amor",
+        "english": "Love"
+    },
+    {
+        "portuguese": "Felicidade",
+        "english": "Happiness"
+    },
+    {
+        "portuguese": "Tristeza",
+        "english": "Sadness"
+    },
+    {
+        "portuguese": "Saúde",
+        "english": "Health"
+    },
+    {
+        "portuguese": "Dinheiro",
+        "english": "Money"
+    },
+    {
+        "portuguese": "Tempo",
+        "english": "Time"
+    },
+    {
+        "portuguese": "Vida",
+        "english": "Life"
+    },
+    {
+        "portuguese": "Morte",
+        "english": "Death"
+    },
+    {
+        "portuguese": "Futuro",
+        "english": "Future"
+    }
+]
